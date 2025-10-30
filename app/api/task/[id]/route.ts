aa667ca94a140fe43eaf0da1ae0e258d31253449
Susheel Rai <susheelrai01@Susheels-MacBook-Pro.local>
2025-10-30 16:37:15 +0530

    fixed the next.15 issue with params aync

--- app/api/task/[id]/route.ts
+++ app/api/task/[id]/route.ts
@@ -2,9 +2,9 @@ import { NextRequest } from "next/server";
 
 export const GET = async (
   request: NextRequest,
-  { params }: { params: { id: string } }
+  context: { params: Promise<{ id: string }> }
 ) => {
-  const { id } = params;
+  const { id } = await context.params; // 👈 await here (this is the fix)
   return new Response(`Hello, this is the task API endpoint for task ${id}!`, {
     status: 200,
   });
