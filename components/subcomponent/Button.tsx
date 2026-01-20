370bcd09a630c95b630c30ded4d74dddc5bc7e06
Susheel Rai <susheelrai01@Susheels-MacBook-Pro.local>
2026-01-21 00:58:23 +0530

    Designed responsiveness and theme change in navbar!

--- components/subcomponent/Button.tsx
+++ components/subcomponent/Button.tsx
@@ -3,10 +3,10 @@ import React from 'react'
 export const Button = ({ text, onClick }: { text: string, onClick: () => void }) => {
   return (
     <button
-      className={`px-4 py-3 m-2 transition-colors duration-300 rounded-xl 
-        ${text === "Sign Up" 
-          ? "bg-btn-accent border-transparent text-black hover:bg-btn-accent-hover" 
-          : "shadow-lg border-b border-neutral-500 hover:bg-neutral-100"}`
+      className={`px-4 py-2 m-2 -translate-x-5 text-[12px] md:text-base font-sans transition-colors duration-300 rounded-xl 
+        ${text === "Sign Up"
+          ? "bg-secondary-foreground border-transparent text-primary-foreground hover:bg-primary"
+          : "shadow-lg border-1 border-border hover:bg-secondary"}`
       }
       onClick={onClick}
     >
--- components/subcomponent/Navbar.tsx
+++ components/subcomponent/Navbar.tsx
@@ -2,6 +2,9 @@
 import { useTheme } from "@/theme/ThemeProvider";
 import { Button } from "./Button"
 import { useRouter } from "next/navigation"
+import { motion } from 'motion/react'
+import { Moon, Sun } from "geist-icons";
+
 
 export const Navbar = () => {
   const { theme, toggleTheme } = useTheme()
@@ -13,15 +16,27 @@ export const Navbar = () => {
   };
 
   return (
-    <nav className="h-24 text-foreground bg-background  border-b shadow-lg rounded-2xl flex items-center justify-between px-4">
+    <nav className="h-20  bg-background border-b border-border  shadow-lg rounded-2xl flex items-center justify-between">
       {/* left section */}
-      <div className='text-2xl mx-8'>
-        <button onClick={toggleTheme} className="border p-2 bg-black text-white dark:bg-white dark:text-black" >switch</button>
-        daycraft.<span className='text-purple-600'>ai</span>
+      <div className='text-lg  md:text-2xl font-sans flex items-center mx-8 md:mx-20'>
+        <motion.button
+          initial={{ opacity: 0, scale: 0.8, rotateY: -20 }}
+          animate={{ opacity: 1, scale: 1, rotateY: 0 }}
+          exit={{ opacity: 0, scale: 0.8, rotateY: 20 }}
+          transition={{ duration: 0.15 }}
+          onClick={toggleTheme}
+
+        >
+          {theme === 'light' ? <Sun className="text-chart-4" size={15} /> : <Moon size={15} className="text-chart-4" />}
+        </motion.button>
+        <button onClick={toggleTheme}>
+          &nbsp;{theme === 'light' ? "Day" : " Night"}
+        </button>
+        Craft.<span className=''>ai</span>
       </div>
 
       {/* right section */}
-      <div className="mx-1 sm:mx-8 flex flex-row sm:flex-row gap-2 sm:gap-4 mt-2 sm:mt-0">
+      <div className="mr-0 md:mr-10">
         <Button text='Sign In' onClick={() => handleButtonClick("signin")} />
         <Button text='Sign Up' onClick={() => handleButtonClick("signup")} />
       </div>
